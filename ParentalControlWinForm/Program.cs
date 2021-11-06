5b224ed0706399c1fcef7fe1be963f7c8feeeb3b
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2021-11-05 22:56:17 -0500

    <HolaMundo>

--- ParentalControlWinForm/Program.cs
+++ ParentalControlWinForm/Program.cs
@@ -18,7 +18,8 @@ namespace ParentalControlWinForm
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
+            //comentario prueba 
         }
     }
 }
