0a6aedb2e2678231bfa256d71d3ff00788489047
Johanna Herrera <johanna.herrera1a@gmail.com>
2021-11-07 10:44:09 -0500

    Proyecto creado

--- ParentalControlWindowsForm/Form1.Designer.cs
+++ ParentalControlWindowsForm/Form1.Designer.cs
@@ -1,15 +1,15 @@
 ﻿
-namespace ParentalControlWinForm
+namespace ParentalControlWindowsForm
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,15 +24,15 @@ namespace ParentalControlWinForm
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.btnSignIn = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.btnRegister = new System.Windows.Forms.Button();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
@@ -41,72 +41,72 @@ namespace ParentalControlWinForm
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(318, 22);
+            this.label1.Location = new System.Drawing.Point(378, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Control Parental";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 108);
+            this.label2.Location = new System.Drawing.Point(262, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Usuario:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(129, 172);
+            this.label3.Location = new System.Drawing.Point(262, 176);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña:";
             // 
-            // btnSignIn
+            // btnLogin
             // 
-            this.btnSignIn.Location = new System.Drawing.Point(262, 318);
-            this.btnSignIn.Name = "btnSignIn";
-            this.btnSignIn.Size = new System.Drawing.Size(103, 23);
-            this.btnSignIn.TabIndex = 3;
-            this.btnSignIn.Text = "Iniciar Sesión";
-            this.btnSignIn.UseVisualStyleBackColor = true;
-            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            this.btnLogin.Location = new System.Drawing.Point(295, 273);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(121, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Iniciar Sesión";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnRegister
             // 
-            this.btnRegister.Location = new System.Drawing.Point(384, 318);
+            this.btnRegister.Location = new System.Drawing.Point(456, 273);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(105, 23);
+            this.btnRegister.Size = new System.Drawing.Size(91, 23);
             this.btnRegister.TabIndex = 4;
             this.btnRegister.Text = "Registrarse";
             this.btnRegister.UseVisualStyleBackColor = true;
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(230, 108);
+            this.txtUser.Location = new System.Drawing.Point(342, 101);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(259, 23);
+            this.txtUser.Size = new System.Drawing.Size(232, 20);
             this.txtUser.TabIndex = 5;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(230, 172);
+            this.txtPassword.Location = new System.Drawing.Point(342, 168);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(259, 23);
+            this.txtPassword.Size = new System.Drawing.Size(232, 20);
             this.txtPassword.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.btnRegister);
-            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -122,7 +122,7 @@ namespace ParentalControlWinForm
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.TextBox txtPassword;
